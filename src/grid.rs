69245bca29a73b33c78f3326b8dd374a0652a8f7
Travis <travis@laptop>
2024-02-16 19:58:43 -0800

    test(Grid): clean up from bounds tests

--- src/grid.rs
+++ src/grid.rs
@@ -9,51 +9,70 @@ pub struct Grid {
 
 impl Grid {
     pub fn empty_from_bounds(
+        nodata: f64,
         left: f64,
         bottom: f64,
         right: f64,
         top: f64,
         resolution: usize,
-        nodata: f64,
     ) -> Self {
         let width = (right - left) as usize / resolution;
         let height = (top - bottom) as usize / resolution;
+
         let data = Array3::from_elem((height, width, 1), nodata);
+
         let x_vals = Array1::linspace(left, right, width);
-        let y_vals = Array1::linspace(bottom, top, height);
         let x = Array2::from_shape_fn((height, width), |(_, j)| x_vals[j]);
+
+        let y_vals = Array1::linspace(bottom, top, height);
         let y = Array2::from_shape_fn((height, width), |(i, _)| y_vals[i]);
-        Grid { data, nodata, x, y }
+
+        Grid { nodata, data, x, y }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn test_from_bounds() {
+    fn equivalent(left: f64, right: f64) -> bool {
+        left.is_nan() && right.is_nan() || left == right
+    }
+
+    #[rstest]
+    #[case(4000, 709, 1350, f64::NAN)]
+    #[case(8000, 354, 675, f64::NEG_INFINITY)]
+    #[case(999, 2842, 5408, f64::MAX)]
+    fn test_from_bounds_resolution(
+        #[case] resolution: usize,
+        #[case] expected_height: usize,
+        #[case] expected_width: usize,
+        #[case] nodata: f64,
+    ) {
         let left = -2221060.;
         let bottom = 523589.;
         let right = 3181702.;
         let top = 3363319.;
-        let resolution = 4000;
-        let nodata = f64::NAN;
 
-        let grid = Grid::empty_from_bounds(left, bottom, right, top, resolution, nodata);
+        let grid = Grid::empty_from_bounds(nodata, left, bottom, right, top, resolution);
 
-        assert_eq!(grid.data.shape(), [709, 1350, 1]);
+        assert_eq!(grid.data.shape(), [expected_height, expected_width, 1]);
 
-        assert!(grid.data[[0, 0, 0]].is_nan());
+        assert!(equivalent(grid.data[[0, 0, 0]], nodata));
         assert_eq!(grid.x[[0, 0]], left);
         assert_eq!(grid.y[[0, 0]], bottom);
 
-        assert!(grid.data[[708, 1349, 0]].is_nan());
-        assert_eq!(grid.x[[708, 1349]], right);
-        assert_eq!(grid.y[[708, 1349]], top);
+        let max_y = expected_height - 1;
+        let max_x = expected_width - 1;
+        assert!(equivalent(grid.data[[max_y, max_x, 0]], nodata));
+        assert_eq!(grid.x[[max_y, max_x]], right);
+        assert_eq!(grid.y[[max_y, max_x]], top);
 
-        assert!(grid.data[[709 / 2, 1350 / 2, 0]].is_nan());
-        assert_eq!(grid.x[[709 / 2, 1350 / 2]].round(), 482324.);
-        assert_eq!(grid.y[[709 / 2, 1350 / 2]].round(), 1943454.);
+        let mid_y = expected_height / 2;
+        let mid_x = expected_width / 2;
+        assert!(equivalent(grid.data[[mid_y, mid_x, 0]], nodata));
+        assert!((grid.x[[mid_y, mid_x]] - ((right + left) / 2.)).abs() < resolution as f64);
+        assert!((grid.y[[mid_y, mid_x]] - ((top + bottom) / 2.)).abs() < resolution as f64);
     }
 }
