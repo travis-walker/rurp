f6129f39f5728077c23923b7e6c6f7b88ef06bee
Travis Walker <travis-walker@users.noreply.github.com>
2024-03-12 22:01:56 -0700

    perf

--- crates/rurp/src/interpolate/inverse_distance_weighting.rs
+++ crates/rurp/src/interpolate/inverse_distance_weighting.rs
@@ -1,36 +1,46 @@
 use crate::grid::Grid;
 use crate::point::Point;
-use geo::EuclideanDistance;
-use kiddo::{KdTree, SquaredEuclidean};
+use kiddo::{KdTree, NearestNeighbour as NearestNeighbor, SquaredEuclidean};
 use rayon::prelude::*;
 
-fn calculate_weight(point_a: &geo::Point, point_b: &geo::Point, power: f64) -> f64 {
-    let distance = point_a.euclidean_distance(point_b);
-    if distance == 0.0 {
-        return 1.;
-    }
-    distance.powf(-power)
-}
-
-fn calculate_interpolated_value(x: f64, y: f64, points: &[&Point], power: f64) -> f64 {
-    let grid_point = geo::Point::new(x, y);
+fn calculate_interpolated_value(
+    neighbors: &[NearestNeighbor<f64, u64>],
+    points: &[Point],
+    power: f64,
+) -> f64 {
     let mut weights_sum = 0.;
-    let weights: Vec<_> = points
+    let weights: Vec<_> = neighbors
         .iter()
-        .map(|&point| {
-            let weight = calculate_weight(&point.into(), &grid_point, power);
+        .map(|neighbor| {
+            let weight = if neighbor.distance == 0.0 {
+                f64::INFINITY
+            } else {
+                // distance is the squared distance
+                // (distance ^ (1/2)) ^ -power == distance ^ -(power / 2)
+                neighbor.distance.powf(power / -2.)
+            };
             weights_sum += weight;
             weight
         })
         .collect();
-    points
-        .par_iter()
-        .zip(weights.par_iter())
-        .map(|(point, weight)| point.values[0] * weight / weights_sum)
+    neighbors
+        .iter()
+        .zip(weights.iter())
+        .map(|(neighbor, weight)| {
+            let point = &points[neighbor.item as usize];
+            point.values[0] * weight / weights_sum
+        })
         .sum()
 }
 
 /// Interpolates to the grid using the Inverse Distance Weighting method.
+///
+/// # Arguments
+/// `grid` - The grid to interpolate to.
+/// `points` - The points to interpolate from.
+/// `power` - The power used in the interpolation. Values are weighted by 1 / distance ^ power.
+/// `radius` - The radius to search for points to interpolate from for each grid point. Points outside of this radius are ignored.
+/// `min_neighbors` - The minimum number of points in radius required to interpolate a value.
 pub fn interpolate(
     grid: &mut Grid,
     points: &[Point],
@@ -52,11 +62,7 @@ pub fn interpolate(
         .for_each(|(x, y, grid_value)| {
             let neighbors = point_tree.within::<SquaredEuclidean>(&[x, y], radius_squared);
             if !neighbors.is_empty() && neighbors.len() >= min_neighbors {
-                let neighbor_points: Vec<&Point> = neighbors
-                    .iter()
-                    .map(|neighbor| &points[neighbor.item as usize])
-                    .collect();
-                *grid_value = calculate_interpolated_value(x, y, &neighbor_points, power);
+                *grid_value = calculate_interpolated_value(&neighbors, points, power);
             }
         });
 }
