0b3586e787ffc20e37e785b10cd13797d61b6869
Travis Walker <travis-walker@users.noreply.github.com>
2024-03-15 01:26:22 -0700

    wip

---
+++ crates/py-rurp/rurp/__init__.py
@@ -0,0 +1,3 @@
+from py_rurp import sum_as_string
+
+__all__ = ["sum_as_string"]
---
+++ crates/py-rurp/tests/test_123.py
@@ -0,0 +1,4 @@
+from py_rurp import sum_as_string
+
+if __name__ == "__main__":
+    assert sum_as_string(1, 2) == "3"
